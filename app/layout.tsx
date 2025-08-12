5c231624004def3d1b07759b843a547e19453b84
Laxman K R <krlaxman03@gmail.com>
2025-08-12 23:43:34 +0530

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Vercel Vibe Coding Agent',
-  description: '',
+  title: 'MathGPT',
+  description: 'ChatGPT with graphical user interface',
 }
 
 export default function RootLayout({
