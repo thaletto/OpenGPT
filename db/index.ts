e6de42ef92f9794d53f2ee46b5af172231f28f25
Laxman K R <krlaxman03@gmail.com>
2025-08-16 01:43:52 +0530

    Remove dotenv dependency and related configuration from db/index.ts; update bun.lock and package.json accordingly

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,2 @@
 import { drizzle } from "drizzle-orm/neon-http";
-import { config } from "dotenv";
-import { usersTable } from "./schema";
-
-config({ path: ".env.local" });
-
 export const db = drizzle(`${process.env.DATABASE_URL}`);
