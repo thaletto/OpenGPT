65b67f432cc61b55bc0eeaf591945220927be370
Laxman K R <krlaxman03@gmail.com>
2025-08-16 11:54:25 +0530

    Refactor Chat component: remove unused TabItem, update imports, and enhance sidebar functionality

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,13 +1,10 @@
 import { Chat } from "@/components/chat/chat";
-import { TabItem, TabContent } from "@/components/tabs";
+import { TabContent } from "@/components/tabs";
 
 export default async function Page() {
   return (
     <>
       <div className="flex flex-col h-screen max-h-screen overflow-hidden p-2 space-x-2 space-y-2">
-        <ul className="flex space-x-5 lg:hidden text-sm tracking-tight mt-1.5 px-1">
-          <TabItem tabId="chat">Chat</TabItem>
-        </ul>
         <div className="flex-1 flex w-full min-h-0 overflow-hidden lg:space-x-2">
           <TabContent
             className="h-full flex-col lg:flex w-full min-h-0"
