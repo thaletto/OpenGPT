3546b75455f31db1fa62fc474f0db2d7020aa7ab
Laxman K R <krlaxman03@gmail.com>
2025-08-26 01:52:07 +0530

    Updated models

--- ai/constants.ts
+++ ai/constants.ts
@@ -1,20 +1,22 @@
-import { type GatewayModelId } from '@ai-sdk/gateway'
+import { type GatewayModelId } from "@ai-sdk/gateway";
 
-export const DEFAULT_MODEL: GatewayModelId[number] = 'google/gemini-2.5-flash'
+export const DEFAULT_MODEL: GatewayModelId[number] = "google/gemini-2.5-flash";
 
 export const SUPPORTED_MODELS: GatewayModelId[] = [
-  'amazon/nova-pro',
-  'anthropic/claude-4-sonnet',
-  'google/gemini-2.5-flash',
-  'moonshotai/kimi-k2',
-  'openai/gpt-4o',
-  'openai/gpt-5',
-  'openai/o4-mini',
-  'xai/grok-3-fast',
-]
+  "google/gemini-2.5-flash",
+  "openai/gpt-5-mini",
+  "openai/gpt-5-nano",
+  "openai/gpt-oss-120b",
+  "moonshotai/kimi-k2",
+  "deepseek/deepseek-r1",
+  "zai/glm-4.5",
+  "perplexity/sonar",
+  "alibaba/qwen-3-235b",
+  "alibaba/qwen3-coder"
+];
 
 export const SUGGESTIONS = [
-  'Help me draft an email.',
-  'Suggest a diet plan for bodybuilding',
-  'Plan a short weekend activity.',
-]
+  "Help me draft an email.",
+  "Suggest a diet plan for bodybuilding",
+  "Plan a short weekend activity.",
+];
